3e63553430d46cec2704f519ae4780f50c50dd91
Unknown <laurocoelhoc@gmail.com>
2018-04-02 02:00:08 -0300

    Finalizado implementação tela Cidade

--- src/main/java/br/com/docrotas/server/controller/UfController.java
+++ src/main/java/br/com/docrotas/server/controller/UfController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.docrotas.server.entity.Uf;
 import br.com.docrotas.server.repository.UfRepository;
 
+@CrossOrigin
 @RestController
 public class UfController {
 
 	@Autowired
 	UfRepository ufRepository;
+	
+	@GetMapping("/uf/{id}")
+	public Uf buscarPorId(@PathVariable Long id) {
+		return ufRepository.findOne(id);
+	}
 
 	@GetMapping("/uf")
 	public Page<Uf> buscarTodos(@RequestParam(value = "pagina", required = true)int pagina,
